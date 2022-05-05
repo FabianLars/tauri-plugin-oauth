e8845e640b2fed3e1e99b68d4ed8eb040fdc5cd3
FabianLars <fabianlars@fabianlars.de>
2022-05-06 01:08:20 +0200

    Finish PoC. inject js in response to read fragment

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use std::{
-    io::{BufReader, Error, ErrorKind, Read, Write},
-    net::{SocketAddr, TcpListener},
-    sync::atomic::{AtomicBool, Ordering},
+    io::{Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
     thread,
-    time::Duration,
 };
 
 use tauri::{
@@ -11,25 +9,26 @@ use tauri::{
     Runtime,
 };
 
-static CANCEL: AtomicBool = AtomicBool::new(false);
+const EXIT: [u8; 4] = [1, 3, 3, 7];
 
 /// Initializes the plugin.
+#[must_use]
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("oauth").build()
 }
 
-/// Start the localhost (using 127.0.0.1) server. Returns the port its listening on.
-/// You need to verify each response you get in your handler function, because of the system-wide localhost port.
+/// Starts the localhost (using 127.0.0.1) server. Returns the port its listening on.
+///
+/// Because of the unprotected localhost port, you _must_ verify the URL in the handler function.
 ///
 /// # Arguments
 ///
-/// * `response` - Optional static html string the user sees after being redirected. Default: "<html><body>Please return to the app.</body></html>".
-/// * `handler` - Closure which gets called on every incoming connection.
+/// * `response` - Optional static html string send to the user after being redirected. Keep it self-contained and as small as possible. Default: `"<html><body>Please return to the app.</body></html>"`.
+/// * `handler` - Closure which will be executed on a successful connection. It receives the full URL as a String.
 ///
 /// # Errors
 ///
-/// - Returns ErrorKind::AlreadyExists if there is an open server already.
-/// - Returns std::io::Error if the server creation fails.
+/// - Returns `std::io::Error` if the server creation fails.
 ///
 /// # Panics
 ///
@@ -38,53 +37,21 @@ pub fn start<F: FnMut(String) + Send + 'static>(
     response: Option<&'static str>,
     mut handler: F,
 ) -> Result<u16, std::io::Error> {
-    if CANCEL.load(Ordering::SeqCst) {
-        return Err(Error::new(
-            ErrorKind::AlreadyExists,
-            "Server already running, call stop() first.",
-        ));
-    }
-
-    CANCEL.store(false, Ordering::SeqCst);
-
     let listener = TcpListener::bind(SocketAddr::from(([127, 0, 0, 1], 0)))?;
-    listener.set_nonblocking(true)?;
 
     let port = listener.local_addr()?.port();
 
     thread::spawn(move || {
         for conn in listener.incoming() {
             match conn {
-                Ok(mut conn) => {
-                    let mut conn_reader = BufReader::new(&conn);
-                    let mut buffer = String::new();
-                    if let Err(io_err) = conn_reader.read_to_string(&mut buffer) {
-                        log::error!("Error reading incoming connection: {}", io_err.to_string());
-                    };
-                    buffer.pop();
-
-                    let response =
-                        response.unwrap_or("<html><body>Please return to the app.</body></html>");
-
-                    // TODO: Test if unwrapping here is safe (enough).
-                    conn.write_all(
-                        format!(
-                            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                            response.len(),
-                            response
-                        )
-                        .as_bytes(),
-                    )
-                    .unwrap();
-                    conn.flush().unwrap();
-
-                    handler(buffer);
-                }
-                Err(err) if err.kind() == ErrorKind::WouldBlock => {
-                    if CANCEL.load(Ordering::Relaxed) {
+                Ok(conn) => {
+                    if let Some(url) = handle_connection(conn, response, port) {
+                        // Using an empty string to communicate that a shutdown was requested.
+                        if !url.is_empty() {
+                            handler(url);
+                        }
+                        // TODO: Check if exiting here is always okay.
                         break;
-                    } else {
-                        thread::sleep(Duration::from_millis(500));
                     }
                 }
                 Err(err) => {
@@ -96,8 +63,89 @@ pub fn start<F: FnMut(String) + Send + 'static>(
     Ok(port)
 }
 
-/// Stop the currently running server.
-/// The server needs up to 500ms to shutdown, but you don't need to wait before you can call [`start`] agian.
-pub fn stop() {
-    CANCEL.store(true, Ordering::SeqCst);
+fn handle_connection(
+    mut conn: TcpStream,
+    response: Option<&'static str>,
+    port: u16,
+) -> Option<String> {
+    let mut buffer = [0; 1024];
+    if let Err(io_err) = conn.read(&mut buffer) {
+        log::error!("Error reading incoming connection: {}", io_err.to_string());
+    };
+    if buffer[..4] == EXIT {
+        return Some(String::new());
+    }
+
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut request = httparse::Request::new(&mut headers);
+    request.parse(&buffer).ok()?;
+
+    let path = request.path.unwrap_or_default();
+
+    if path == "/exit" {
+        return Some(String::new());
+    };
+
+    let mut is_localhost = false;
+
+    for header in &headers {
+        if header.name == "Full-Url" {
+            return Some(String::from_utf8_lossy(header.value).to_string());
+        } else if header.name == "Host" {
+            is_localhost = String::from_utf8_lossy(header.value).starts_with("localhost");
+        }
+    }
+    if path == "/cb" {
+        log::error!(
+            "Client fetched callback path but the request didn't contain the expected header."
+        );
+    }
+
+    let script = format!(
+        r#"<script>fetch("http://{}:{}/cb",{{headers:{{"Full-Url":window.location.href}}}})</script>"#,
+        if is_localhost {
+            "localhost"
+        } else {
+            "127.0.0.1"
+        },
+        port
+    );
+    let response = match response {
+        Some(s) if s.contains("<head>") => s.replace("<head>", &format!("<head>{}", script)),
+        Some(s) => s.replace("<body>", &format!("<head>{}</head><body>", script)),
+        None => format!(
+            "<html><head>{}</head><body>Please return to the app.</body></html>",
+            script
+        ),
+    };
+
+    // TODO: Test if unwrapping here is safe (enough).
+    conn.write_all(
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            response.len(),
+            response
+        )
+        .as_bytes(),
+    )
+    .unwrap();
+    conn.flush().unwrap();
+
+    None
+}
+
+/// Stops the currently running server behind the provided port without executing the handler.
+/// Alternatively you can send a request to http://127.0.0.1:port/exit
+///
+/// # Errors
+///
+/// - Returns `std::io::Error` if the server couldn't be reached.
+pub fn cancel(port: u16) -> Result<(), std::io::Error> {
+    // Using tcp instead of something global-ish like an AtomicBool,
+    // so we don't have to dive into the set_nonblocking madness.
+    let mut stream = TcpStream::connect(SocketAddr::from(([127, 0, 0, 1], port)))?;
+    stream.write_all(&EXIT)?;
+    stream.flush()?;
+
+    Ok(())
 }
