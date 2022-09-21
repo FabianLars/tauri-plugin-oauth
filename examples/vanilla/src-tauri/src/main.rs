0c354f5aefa93b1703f7757b36919f0a4a16ff67
FabianLars <fabianlars@fabianlars.de>
2022-09-22 00:51:53 +0200

    add rust side of example

--- examples/vanilla/src-tauri/src/main.rs
+++ examples/vanilla/src-tauri/src/main.rs
@@ -3,9 +3,21 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{command, Window};
+use tauri_plugin_oauth::start;
+
+#[command]
+async fn start_server(window: Window) -> Result<u16, String> {
+  start(None, move |url| {
+    // Because of the unprotected localhost port, you must verify the URL here.
+    // Preferebly send back only the token, or nothing at all if you can handle everything else in Rust.
+    let _ = window.emit("redirect_uri", url);
+  }).map_err(|err| err.to_string())
+}
+
 fn main() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_oauth::init())
+    .invoke_handler(tauri::generate_handler![start_server])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
