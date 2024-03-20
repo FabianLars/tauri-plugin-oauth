c93f59d0623beba2c0a61b0089f50ad78c2f4223
fabianlars <fabianlars@fabianlars.de>
2024-03-20 11:55:14 +0100

    disable android/ios build script

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ const COMMANDS: &[&str] = &["start", "cancel"];
 
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
-        .android_path("android")
-        .ios_path("ios")
+        //.android_path("android")
+        //.ios_path("ios")
         .build();
 }
